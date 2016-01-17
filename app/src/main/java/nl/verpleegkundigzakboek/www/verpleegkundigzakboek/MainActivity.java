3fb61aeb1aa7f0efd6b3dedd059556f13a8bc54d
gerwinvis <gerwinvis@gmail.com>
2016-01-17 13:51:05 +0100

    Initial commit

---
+++ app/src/main/java/nl/verpleegkundigzakboek/www/verpleegkundigzakboek/MainActivity.java
@@ -0,0 +1,159 @@
+package nl.verpleegkundigzakboek.www.verpleegkundigzakboek;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView = null;
+    Toolbar toolbar = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set the fragment initially
+        MainFragment fragment = new MainFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String facebookUrl = "http://www.facebook.com/verpleegkundigzakboek";
+                try {
+                    int versionCode = getPackageManager().getPackageInfo("com.facebook.katana", 0).versionCode;
+                    if (versionCode >= 3002850) {
+                        Uri uri = Uri.parse("fb://facewebmodal/f?href=" + facebookUrl);
+                        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                    } else {
+                        Uri uri = Uri.parse("fb://page/1436255413303592");
+                        startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse(facebookUrl));
+                    startActivity(viewIntent);
+                }
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Disclaimer fragment = new Disclaimer();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+        }
+
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+        //set the fragment initially
+            MainFragment fragment = new MainFragment();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            //handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Zakkaartjes fragment = new Zakkaartjes();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_slideshow) {
+            Rekenen fragment = new Rekenen();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+
+        } else if (id == R.id.nav_manage) {
+            Intent intent = new Intent(this, Notities.class);
+            startActivity(intent);
+            finish();
+
+        } else if (id == R.id.nav_share) {
+            Protocollen fragment = new Protocollen();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+
+
+        } else if (id == R.id.nav_send) {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                    "mailto","info@verpleegkundigzakboek.nl", null));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email van verpleegkundig zakboek app");
+            startActivity(Intent.createChooser(emailIntent, "Verzend e-mail"));
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/nl/verpleegkundigzakboek/www/verpleegkundigzakboek/NotesProvider.java
@@ -0,0 +1,75 @@
+package nl.verpleegkundigzakboek.www.verpleegkundigzakboek;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class NotesProvider extends ContentProvider{
+
+    private static final String AUTHORITY = "nl.verpleegkundigzakboek.www.verpleegkundigzakboek.NotesProvider";
+    private static final String BASE_PATH = "notes";
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    // Constant to identify the requested operation
+    private static final int NOTES = 1;
+    private static final int NOTES_ID = 2;
+
+    private static final UriMatcher uriMatcher =
+            new UriMatcher(UriMatcher.NO_MATCH);
+
+    public static final String CONTENT_ITEM_TYPE = "Note";
+
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH +  "/#", NOTES_ID);
+    }
+
+    private SQLiteDatabase database;
+
+    @Override
+    public boolean onCreate() {
+
+        DBOpenhelper helper = new DBOpenhelper(getContext());
+        database = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        if (uriMatcher.match(uri) == NOTES_ID) {
+            selection = DBOpenhelper.NOTE_ID + "=" + uri.getLastPathSegment();
+        }
+
+        return database.query(DBOpenhelper.TABLE_NOTES, DBOpenhelper.ALL_COLUMNS,
+                selection, null, null, null,
+                DBOpenhelper.NOTE_CREATED + " DESC");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long id = database.insert(DBOpenhelper.TABLE_NOTES,
+                null, values);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return database.delete(DBOpenhelper.TABLE_NOTES, selection, selectionArgs);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return database.update(DBOpenhelper.TABLE_NOTES,
+                values, selection, selectionArgs);
+    }
+}
